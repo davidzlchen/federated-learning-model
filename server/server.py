a1a8c500c5f4ea98ffa99516c5f92a8c2708c544
David Chen <davidzchen@utexas.edu>
2020-02-06 17:25:05 -0600

    add flask-mqtt example code

--- server/server.py
+++ server/server.py
@@ -1,7 +1,26 @@
 from flask import Flask
+from flask_mqtt import Mqtt
 
 app = Flask(__name__)
+app.config['MQTT_BROKER_URL'] = 'localhost'
+app.config['MQTT_BROKER_PORT'] = 1883
+#app.config['MQTT_USERNAME'] = 'user'
+#app.config['MQTT_PASSWORD'] = 'secret'
+app.config['MQTT_REFRESH_TIME'] = 1.0  # refresh time in seconds
+mqtt = Mqtt(app)
 
 @app.route('/')
-def get_home():
-    return '', 200
+def index():
+    return render_template('index.html')
+
+@mqtt.on_connect()
+def handle_connect(client, userdata, flags, rc):
+    mqtt.subscribe('home/mytopic')
+
+@mqtt.on_message()
+def handle_mqtt_message(client, userdata, message):
+    data = dict(
+        topic=message.topic,
+        payload=message.payload.decode()
+    )
+    print(data)
