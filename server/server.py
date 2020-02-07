927ec3aaca7f88006170445dc96017b9785f34ea
Avishka Suduwa Dewage <avishkas5@gmail.com>
2020-02-07 01:38:49 -0600

    basic server and mqtt server running

--- server/server.py
+++ server/server.py
@@ -1,6 +1,10 @@
 from flask import Flask
 from flask_mqtt import Mqtt
 
+import pandas as pd
+import pickle
+import torch
+
 app = Flask(__name__)
 app.config['MQTT_BROKER_URL'] = 'localhost'
 app.config['MQTT_BROKER_PORT'] = 1883
@@ -9,13 +13,17 @@ app.config['MQTT_BROKER_PORT'] = 1883
 app.config['MQTT_REFRESH_TIME'] = 1.0  # refresh time in seconds
 mqtt = Mqtt(app)
 
+
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @mqtt.on_connect()
 def handle_connect(client, userdata, flags, rc):
-    mqtt.subscribe('home/mytopic')
+    mqtt.subscribe('central/getdata')
+    
 
 @mqtt.on_message()
 def handle_mqtt_message(client, userdata, message):
@@ -23,4 +31,24 @@ def handle_mqtt_message(client, userdata, message):
         topic=message.topic,
         payload=message.payload.decode()
     )
+
+
+    if data.topic == 'central/getdata':
+        #convert pickle
+        new_data = pd.read_pickle(data.payload)
+
+        #update model
+
+
+
+        #pickle the model
+
+        #send model back down
+
+
+
     print(data)
+
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=5000)
