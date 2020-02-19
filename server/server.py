a002d08f6942fa1557cdfc36eed9cf9f43afb56c
Avishka Suduwa Dewage <avishkas5@gmail.com>
2020-02-18 21:23:52 -0600

    updated send data

--- server/server.py
+++ server/server.py
@@ -70,8 +70,10 @@ def send_network_model(payload):
 
 @app.route('/')
 def index():
-    network.run(clientDataBlock)
-    send_network_model(network)
+    # network.run(clientDataBlock)
+    # send_network_model(network)
+
+    saveImages()
 
     return "training model"
 
