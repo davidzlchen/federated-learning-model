af3c03590ce2590915efbb4fdcec92e5955c0046
Avishka Suduwa Dewage <avishkas5@gmail.com>
2020-03-02 15:19:52 -0600

    more enums

--- common/networkblock.py
+++ common/networkblock.py
@@ -1,11 +1,16 @@
 from utils.model_helper import get_state_dictionary
 import utils.constants as constants
+from enum import Enum
+
+class NetworkStatus(Enum):
+    STALE=1
+    NEW=2
 
 
 class Networkblock(object):
     def __init__(self):
         self.network_string = ""
-        self.network_status = constants.NETWORK_STALE
+        self.network_status = NetworkStatus.STALE
         self.state_dict = None
 
     def add_network_chunk(self, chunk):
@@ -13,9 +18,9 @@ class Networkblock(object):
 
     def reset_network_data(self):
         self.network_string = ""
-        self.network_status = constants.NETWORK_STALE
+        self.network_status = NetworkStatus.STALE
 
     def reconstruct_state_dict(self):
-        self.network_status = constants.NETWORK_NEW
+        self.network_status = NetworkStatus.NEW
         self.state_dict = get_state_dictionary(network_string=self.network_string)
         return self.state_dict
