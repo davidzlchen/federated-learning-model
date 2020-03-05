[server] Fix broken server code for correct averaging

common/aggregation_scheme.py
@@ -2,9 +2,11 @@ from utils import constants
 import numpy as np
 from enum import Enum
 
+
 class AggregationScheme(Enum):
-    AVERAGE=1
-    WEIGHTED_AVERAGE=2 
+    AVERAGE = 1
+    WEIGHTED_AVERAGE = 2
+
 
 DEFAULT_AGGREGATION_SCHEME = AggregationScheme.AVERAGE
 
@@ -16,21 +18,20 @@ def get_aggregation_scheme(server_model, CLIENT_IDS, CLIENT_NETWORKS):
 
 
 def get_average(server_model, CLIENT_IDS, CLIENT_NETWORKS):
-        fc_state_dict = server_model.model.fc.state_dict()
+    fc_state_dict = server_model.model.fc.state_dict()
 
-        weights = np.zeros(fc_state_dict['weight'].size())
-        bias = np.zeros(fc_state_dict['bias'].size())
+    weights = np.zeros(fc_state_dict['weight'].size())
+    bias = np.zeros(fc_state_dict['bias'].size())
 
-        for client_id in CLIENT_IDS:
+    for client_id in CLIENT_IDS:
 
-            fc_state_dict = CLIENT_NETWORKS[client_id].state_dict
-            weights = np.add(weights, fc_state_dict['weight'])
-            bias = np.add(bias, fc_state_dict['bias'])
+        fc_state_dict = CLIENT_NETWORKS[client_id].state_dict
+        weights = np.add(weights, fc_state_dict['weight'])
+        bias = np.add(bias, fc_state_dict['bias'])
 
-        num_clients = len(CLIENT_IDS)
+    num_clients = len(CLIENT_IDS)
 
-        weights = np.divide(weights, num_clients)
-        bias = np.divide(bias, num_clients)
+    weights = np.divide(weights, num_clients)
+    bias = np.divide(bias, num_clients)
 
-        return (weights, bias)
-
+    return (weights, bias)

utils/model_helper.py
@@ -13,6 +13,9 @@ def get_state_dictionary(
     if USE_LOCAL_NETWORK_CHECKPOINT:
         checkpoint = torch.load(path)
     else:
-        checkpoint = torch.load(BytesIO(base64.decodebytes(network_string.encode())))
+        checkpoint = torch.load(
+            BytesIO(
+                base64.decodebytes(
+                    network_string.encode())))
 
     return checkpoint