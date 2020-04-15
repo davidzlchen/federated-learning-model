b78b753514466cb3ecf3516b9f5ce75e3f5cf878
David Chen <davidzchen@utexas.edu>
2020-04-15 19:38:18 -0400

    [website] add websocket functionality

--- website/src/runs/Run.tsx
+++ website/src/runs/Run.tsx
@@ -1,15 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Run.css";
 
 import { BrowserRouter as Router, Route, useParams } from "react-router-dom";
+import socketIOClient from "socket.io-client";
 
-function Run() {
-  let { runId } = useParams();
-  return (
-    <div>
-      <h3>Requested run id: {runId}</h3>
-    </div>
-  );
+type RunProps = {};
+
+type RunState = {
+  response: Object;
+  endpoint: string;
+};
+
+class Run extends Component<RunProps, RunState> {
+  constructor(props: RunProps) {
+    super(props);
+    this.state = {
+      response: false,
+      endpoint: "localhost:5000",
+    };
+  }
+
+  componentDidMount() {
+    const { endpoint } = this.state;
+    const socket = socketIOClient(endpoint);
+    socket.on("FromAPI", (data: Object) => this.setState({ response: data }));
+  }
+
+  render() {
+    const { response } = this.state;
+    return (
+      <div>
+        <h3>Requested run id: {response}</h3>
+      </div>
+    );
+  }
 }
 
 export default Run;
