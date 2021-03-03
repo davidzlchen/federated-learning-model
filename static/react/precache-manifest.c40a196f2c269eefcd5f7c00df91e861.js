a6d2c0edeaca89e14765673ce4f4258fac21c851
David Z. Chen <davidzchen@utexas.edu>
2021-03-03 15:38:28 -0800

    [website] Change button to submit to localhost:5000 instead of hard coded ip address (#38)
    Co-authored-by: David Chen <davidzchen@fb.com>

---
+++ static/react/precache-manifest.c40a196f2c269eefcd5f7c00df91e861.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "337a2e8f679465b2d894260842de7397",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "387728e3836ae716d87e",
+    "url": "/static/react/css/2.8f18a42f.chunk.css"
+  },
+  {
+    "revision": "466f190e0a7413069eeb",
+    "url": "/static/react/css/main.761dfbb3.chunk.css"
+  },
+  {
+    "revision": "387728e3836ae716d87e",
+    "url": "/static/react/js/2.c58b7f4a.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/static/react/js/2.c58b7f4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "466f190e0a7413069eeb",
+    "url": "/static/react/js/main.e61c098f.chunk.js"
+  },
+  {
+    "revision": "f14fe7d5b5dfd7247f10",
+    "url": "/static/react/js/runtime-main.4fa9fdd5.js"
+  }
+]);
